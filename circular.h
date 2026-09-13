5ea533f5dee64024c6a153a6bd966df0db5f8b02
NandaAmille <149482287+NandaAmille@users.noreply.github.com>
2026-09-13 20:28:36 -0300

    Add files via upload
    commit 2

--- circular.h
+++ circular.h
@@ -1,29 +1,31 @@
-#ifndef PRODUTO_H
-#define PRODUTO_H
+#ifndef CIRCULAR_H
+#define CIRCULAR_H
+#include "produto.h"
+
+typedef struct{
+    Produto produto;
+    struct No *prox_item;
+}No;
 
 typedef struct {
-    unsigned int id;
-    char nome[50];
-    unsigned int qtd;
-    float preco;
-    char validade[10];
-    struct Produto *prox_item;
-}Produto;
+    No *inicio;
+    No *fim;
+}Lista_circular;
 
 void inserir_inicio();
 
-void inserir_fim(Produto *inicio);
+void inserir_fim(Lista_circular *lista);
 
-void remover(Produto *inicio);
+void remover(Lista_circular *lista);
 
-void buscar_por_nome(Produto *inicio, char texto[]);
+void buscar_por_nome(Lista_circular *lista, char texto[]);
     
-void contar_produtos(Produto *inicio);
+void contar_produtos(Lista_circular *lista);
 
-void atualizacao_qtd(Produto *inicio, unsigned int idt, unsigned int nova_qtd);
+void atualizacao_qtd(Lista_circular *lista, unsigned int idt, unsigned int nova_qtd);
 
-void exibir(Produto *inicio);
+void exibir(Lista_circular *lista);
 
-void esvaziar_lista(Produto *inicio);
+void esvaziar_lista(Lista_circular *lista);
 
 #endif
